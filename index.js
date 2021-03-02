a915b766bce32941d088d286bbd31c946ad478b0
Ушаков Сергей Дмитриевич <SDUshakov@sberbank.ru>
2021-03-02 21:23:46 +0300

    need to handle degree more than 2

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import { parsing } from './parsing.js';
 import { solution } from './solution.js';
 
 const main = () => {
+    if (process.argv.length !== 3)
+    {
+        console.log("Usage: ./computor \"<pol> = <pol>\"");
+        process.exit(0);
+    }
+
     const equation = process.argv[2];
 
     solution(parsing(equation));
--- solution.js
+++ solution.js
@@ -1,15 +1,45 @@
+const abs = (number) => number < 0 ? -number : number;
+
+const sqrt = (number) => {
+    let left = 0;
+    let right = 0;
+    let medium = 0;
+
+    right = number / 2
+    while (right - left > 0.000000001) {
+        medium = left + (right - left) / 2
+        if (medium * medium < number) {
+            left = medium
+        } else {
+            right = medium
+        }
+    }
+    return left
+};
+
+const makeReduceForm = (data) => {
+    const c = data.c ? `${data.c > 0 ? '' : '-'}${abs(data.c)} * X^0 ` : '';
+    const b = data.b ? `${data.b > 0 ? '+' : '-'} ${abs(data.b)} * X^1 ` : '';
+    const a = data.a ? `${data.a > 0 ? '+' : '-'} ${abs(data.a)} * X^2 ` : '';
+    return `${c}${b}${a}= 0`;
+};
+
 export const solution = (data) => {
-    console.log(data);
     const D = data.b**2 - 4 * data.a * data.c;
-    console.log(D);
+    console.log('Reduced form:', makeReduceForm(data));
+    console.log('Polynomial degree:', data.polynomialDegree);
+
+    if (!data.a) {
+        console.log('The solution is:', -data.c/data.b);
+        return;
+    }
 
     if (D > 0) {
-        const x1 = (-data.b + Math.sqrt(D)) / (2 * data.a);
-        const x2 = (-data.b - Math.sqrt(D)) / (2 * data.a);
-        console.log('Polynomial degree:', data.polynomialDegree);
+        const x1 = (-data.b + sqrt(D)) / (2 * data.a);
+        const x2 = (-data.b - sqrt(D)) / (2 * data.a);
         console.log('Discriminant is strictly positive, the two solutions are:');
-        console.log(x1);
         console.log(x2);
+        console.log(x1);
     } else if (D === 0) {
         const x = -data.b/ (2 * data.a);
         console.log(x);
